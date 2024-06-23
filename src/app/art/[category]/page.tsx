2ce3bcb2100df406736bb504a6310fd5e3fa4274
microdotmatrix <liquid.knives.2@gmail.com>
2024-06-22 23:37:56 -0500

    testing server rendered modal

--- src/app/art/[category]/page.tsx
+++ src/app/art/[category]/page.tsx
@@ -1,9 +1,17 @@
 import { Suspense } from "react";
 import { Orbit } from "@/components/loading";
-import { getDigitalArt, getDrawings, getPaintings } from "@/server/db/query";
+import {
+  getDigitalArt,
+  getDrawings,
+  getPaintings,
+  getPaintingById,
+  getDrawingById,
+  getDigitalArtById,
+} from "@/server/db/query";
 import clsx from "clsx";
 import { GalleryCard } from "@/components/art/card";
 import { ArtModal } from "@/components/art/modal";
+import Image from "next/image";
 
 type searchParams = Record<string, string> | null | undefined;
 
@@ -96,9 +104,41 @@ async function Artwork({
             </div>
           }
         >
-          <ArtModal id={itemId} category={category} />
+          <div className="z-100 fixed inset-0 grid h-full w-full place-content-center bg-black/50 backdrop-blur-md">
+            <ImageView id={itemId} category={category} />
+          </div>
         </Suspense>
       )}
+    </div>
+  );
+}
+
+async function ImageView({ id, category }: { id: string; category: string }) {
+  let art;
+  if (category === "paintings") {
+    art = await getPaintingById(id);
+  }
+  if (category === "drawings") {
+    art = await getDrawingById(id);
+  }
+  if (category === "digital") {
+    art = await getDigitalArtById(id);
+  }
+  if (!category || category === undefined) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Image
+        src={`${art?.url}?tr=w-960,h-960`}
+        alt={art?.title}
+        height={960}
+        width={960}
+        loading="lazy"
+        className="max-h-[80vh] max-w-full overflow-hidden object-contain"
+        sizes="(min-width: 1024px) 960px, (min-width: 768px) 800px, 100vw"
+      />
     </div>
   );
 }
